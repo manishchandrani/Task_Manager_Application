cb5c967a113a34eaefa94d17fbc4e219c365c000
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-29 15:27:56 -0500

    Added router for edit and delete tasks

--- routes/task.js
+++ routes/task.js
@@ -8,7 +8,6 @@ const taskRepo = require('../controllers/taskRepo');
 // });
 
 /* GET all tasks*/
-
 router.get('/', async (req, res, next) => {
   try {
     const allTaskData = await taskRepo.findAll();
@@ -50,5 +49,57 @@ router.get('/:id', async (req, res, next) => {
     next(error);
   }
 });
+
+/* GET Edit movie Page */
+router.get('/:id/edit', async (req, res, next) => {
+  const taskData = await taskRepo.findById(req.params.id);
+  res.render('edit', { task: taskData, title: 'Edit Task', buttonText: 'Edit task', actionURL: 'edit' });
+});
+
+/* POST Updated task Details from Edit Task Page */
+router.post('/:id/edit', async (req, res, next) => {
+  try {
+    const { title, description, dueDate, completed } = req.body;
+    const taskId = req.params.id;
+    const updatedTask = { title, description, dueDate, completed };
+    await taskRepo.updateTask(taskId, updatedTask);
+    res.redirect('/tasks');
+  } catch (error) {
+    console.error("Error updating task:", error);
+    res.status(500).send("Error updating task");
+  }
+});
+
+/* GET Task Confirm Delete Page */
+router.get('/:id/delete', async (req, res, next) => {
+  try {
+    const taskData = await taskRepo.findById(req.params.id);
+    if (taskData) {
+      res.render('delete', { task: taskData });
+    } else {
+      res.status(404).send('Task not found');
+    }
+  } catch (error) {
+    console.error("Error fetching task for deletion:", error);
+    res.status(500).send("Error fetching task for deletion");
+  }
+});
+
+/* POST Task Delete */
+router.post('/:id/delete', async (req, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const deletedCount = await taskRepo.deleteTask(taskId);
+    if (deletedCount > 0) {
+      res.redirect('/tasks');
+    } else {
+      res.status(404).send('Task not found');
+    }
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    res.status(500).send("Error deleting task");
+  }
+});
+
 
 module.exports = router;
