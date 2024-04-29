28c792c9248d9c6c222b491eb66d7e66021ca9f5
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-29 13:19:42 -0500

    Created Task data model under models directory

---
+++ models/task.js
@@ -0,0 +1,11 @@
+class Task {
+    constructor(id, title, description, dueDate, completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+}
+
+module.exports = Task;
