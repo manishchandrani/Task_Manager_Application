9f5fe87dc34c8ec3f0311ac34cd156140de428fa
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-30 19:36:47 -0500

    Added local authentication strategy with Passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('../models/user');
+
+
+async function localAuthUser(email,password,done){
+  try{
+    const aUser = await User.findOne({email: email});
+    if(!aUser){
+      return done(null, false);
+    }
+    const isMatch = await aUser.matchPassword(password);
+    if(!isMatch){
+      return done(null,false);
+    }
+    return done(null, aUser);
+  } catch(error){
+    console.log(error);
+    return done(error,false);
+  }
+};
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, localAuthUser));
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
