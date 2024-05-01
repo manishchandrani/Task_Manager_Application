ce6c5e8ae30e2a2fac4dd56684445ab14a361be9
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-30 23:42:02 -0500

    Added express validation for login page and signup page

--- routes/auth.js
+++ routes/auth.js
@@ -2,22 +2,23 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 
-async function localAuthUser(email,password,done){
-  try{
-    const aUser = await User.findOne({email: email});
-    if(!aUser){
+async function localAuthUser(email, password, done) {
+  try {
+    const aUser = await User.findOne({ email: email });
+    if (!aUser) {
       return done(null, false);
     }
     const isMatch = await aUser.matchPassword(password);
-    if(!isMatch){
-      return done(null,false);
+    if (!isMatch) {
+      return done(null, false);
     }
     return done(null, aUser);
-  } catch(error){
+  } catch (error) {
     console.log(error);
-    return done(error,false);
+    return done(error, false);
   }
 };
 
@@ -26,45 +27,66 @@ passport.use(new LocalStrategy({
   passwordField: 'password'
 }, localAuthUser));
 
-passport.serializeUser(function(user, done){
-  done(null,user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function(user,done){
-  done(null,user);
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-/*POST local login */
-router.post('/login/password', passport.authenticate('local',{
-  successRedirect: '/tasks',
-  failureRedirect: '/login',
-}));
+/* POST login page */
+router.post('/login/password', [
+  body('email').trim().isEmail().normalizeEmail().escape().withMessage('Please enter Email'),
+  body('password').trim().notEmpty().escape().withMessage('Please enter Password')
+], (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('login', { msg: errors.array() });
+  }
+
+  passport.authenticate('local', {
+    successRedirect: '/tasks',
+    failureRedirect: '/login',
+  })(req, res, next);
+});
+
 
 /*POST logut*/
-router.post('/logout', function(req,res,next){
-  req.logout(function(error){
-    if(error){return next(error);}
+router.post('/logout', function (req, res, next) {
+  req.logout(function (error) {
+    if (error) { return next(error); }
     res.redirect('/login');
   })
 });
 
 /*GET signup form */
-router.get('/signup', function(req,res,next){
+router.get('/signup', function (req, res, next) {
   res.render('signup');
 });
 
-/* POST signup page*/
-
-router.post('/signup', async function(req, res, next) {
+/* POST signup page */
+router.post('/signup', [
+  body('name').trim().notEmpty().escape().withMessage('Please enter your name'),
+  body('email').trim().isEmail().normalizeEmail().escape().withMessage('Please enter a valid email'),
+  body('password').trim().notEmpty().escape().withMessage('Please enter a password'),
+  body('confirmPassword').trim().notEmpty().escape().withMessage('Please confirm your password')
+], async function (req, res, next) {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('signup', { msg: errors.array() });
+    }
+
     if (req.body.password !== req.body.confirmPassword) {
       return res.render('signup', { passwordError: "Passwords do not match" });
-    }    
+    }
+
     let newUser = new User({
       name: req.body.name,
       email: req.body.email,
@@ -73,16 +95,17 @@ router.post('/signup', async function(req, res, next) {
 
     let savedDoc = await newUser.save();
 
-    req.login(savedDoc, function(err) {
+    req.login(savedDoc, function (err) {
       if (err) {
         return next(err);
       }
       return res.redirect('/login');
     });
   } catch (error) {
-    return res.render('signup', { error : "Please Enter Valid Email" });
+    return res.render('signup', { error: "Please Enter Valid Email" });
   }
 });
+
 
 
 
